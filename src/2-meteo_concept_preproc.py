037b167a140c43081c5d08ae1fa7cddfccb29d82
Zenobiatoon <125663840+Zenobiatoon@users.noreply.github.com>
2023-08-20 18:03:41 +0200

    Add files via upload

---
+++ src/2-meteo_concept_preproc.py
@@ -0,0 +1,37 @@
+# Librairies
+import os
+import json
+import pandas as pd
+from datetime import datetime
+from os.path import dirname, abspath
+
+
+# Création de la date du process
+now = datetime.now()
+dt_fic = now.strftime("%Y-%m-%d")
+
+# Répertoire de l'extraction
+in_data_dir = dirname(dirname(abspath(__file__)))  # chemin
+
+# Chargement de la BDD météo et filtre sur la région Bretagne
+json = pd.read_json(in_data_dir +"/data/raw/meteo_concept_dt_val_"+ dt_fic+".json")
+json['date model']= pd.to_datetime(json['date_val'])
+bretagne = json.loc[json['region']=='Bretagne'].reset_index(drop=True)
+
+
+# Calcul de la 1ère variable explicative : "moyenne des températures" en K 
+bretagne['feat_tmean'] = bretagne.apply(lambda x: ((x['tmax']+x['tmin'])/2 + 273.15), axis=1)
+
+# Variables nécesssaires
+bretagne=bretagne[['region','localite','code_insee','datetime','day','date model','feat_tmean']]
+
+# Calcul de la 2ème variable explicative :
+bretagne['feat_month']= bretagne.datetime.dt.month
+
+# Calcul de la 3ème variable explicative :
+bretagne['feat_day_of_week']= bretagne.datetime.dt.day_of_week
+
+# Mise en forme de la date de prédiction
+bretagne['date prediction'] = bretagne['datetime'].dt.strftime("%Y-%m-%d")
+bretagne.to_json(in_data_dir +"/data/processed/processed_bretagne_"+ dt_fic+".json", orient="records")
+
---
+++ src/3-meteo_concept_model.py
@@ -0,0 +1,80 @@
+# Librairies
+import os
+import json
+import keras
+import numpy as np
+import pandas as pd
+from datetime import datetime
+from os.path import dirname, abspath
+from tcn import TCN, tcn_full_summary
+from tensorflow.keras.models import model_from_json
+
+
+# Création de la date du process
+now = datetime.now()
+dt_fic = now.strftime("%Y-%m-%d")
+
+# Répertoires 
+in_data_dir = dirname(dirname(abspath(__file__)))  + '/data/processed/' # BDD processed data
+in_model = dirname(dirname(abspath(__file__)))  + '/model/' # Paraètres modèles
+
+# Chargement de la BDD
+bretagne = pd.read_json(in_data_dir +"processed_bretagne_"+ dt_fic+".json", orient = 'columns')
+bretagne['date model'] = dt_fic
+
+# Vecteur des variables nécessaires au modèle
+bretagne_array= bretagne[['feat_tmean','feat_month','feat_day_of_week']].to_numpy()
+
+# Mise à l'échelle des données du vecteur
+from sklearn.preprocessing import RobustScaler
+RS = RobustScaler()
+bretagne_scaled= RS.fit_transform(bretagne_array)
+
+# Création de la matrice input du modèle
+X_data = np.zeros(shape=(len(bretagne_scaled),14,3))
+
+for j in (range(len(bretagne_scaled))): 
+    X_data[j,:,:] = bretagne_scaled[j,:]
+
+# Découpage des données par séquence de 14 jours
+timesteps, input_dim = 14, 3
+output_dim = 8
+
+# Définition des paramètres du modèle
+def get_model():
+    inputs=keras.layers.Input(shape=(timesteps, input_dim), name="Input")
+    layer_tcn=TCN(nb_filters=32,kernel_size=6, nb_stacks=1, return_sequences=True, dilations=[1, 2, 4, 8],name='TCN')
+    layer_Dense=keras.layers.Dense(output_dim, name='Dense')
+    x=layer_tcn(inputs)
+    outputs=layer_Dense(x)
+    model = keras.models.Model(inputs = inputs, outputs = outputs, name='Model_Bret')
+    return model
+
+model = get_model()
+
+# Chargement du modèle développé sur la Bretagne
+loaded_json = open(in_model + '/model.json', 'r').read()
+reloaded_model = model_from_json(loaded_json, custom_objects={'TCN': TCN})
+tcn_full_summary(model, expand_residual_blocks=False)
+
+# Chargement des poids
+reloaded_model.load_weights(in_model + 'weights.h5')
+
+# Prédiction
+y_pred=reloaded_model.predict(X_data)
+
+# Création du dataframe en sortie 
+df_tmp = pd.concat([bretagne[['localite','date prediction','date model']],pd.DataFrame(y_pred[:,0,0], columns =['conso(MW)'])],axis=1)
+
+df =  pd.DataFrame()
+for i in np.unique(df_tmp.localite):
+    df = pd.concat([df,df_tmp[df_tmp['localite']==i].reset_index(drop=True)])
+
+# Mise en forme
+df['jour predit'] = df.index.rename('jour predit') + 1
+df = df.reset_index(names='id jour')
+df = df[['localite','date prediction','jour predit','id jour','conso(MW)', 'date model']]
+
+df.to_json(in_data_dir +"model_bretagne_"+ dt_fic +".json", orient="records")
+
+print(df)
