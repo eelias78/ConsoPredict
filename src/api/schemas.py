23e843ffd35f94da5067d5cdf8647161ceb4ce5e
Zenobiatoon <zinebslitine.com>
2023-08-31 10:52:53 +0000

    Mise à jour schemas.py

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -1,12 +1,13 @@
 from pydantic import BaseModel, Field
 
 
-class AuthDetails(BaseModel):
-    Identifiant: str
-    MotDePasse: str
-
 class Profil(BaseModel):
     Nom:str = Field(min_length=1, max_length=100)
     Prenom:str = Field(min_length=1, max_length=100)
     Email:str = Field(min_length=1, max_length=100)
     Alias:str = Field(min_length=1, max_length=100)
+    MotdePasse:str = Field(min_length=1, max_length=100)
+
+class AuthDetails(BaseModel):
+    Alias: str 
+    MotdePasse: str
