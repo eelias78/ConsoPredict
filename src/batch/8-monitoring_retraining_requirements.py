5c19318195b90ed3ddb2e29e3f087b0fc579521c
Zenobiatoon <zinebslitine.com>
2023-10-02 12:59:47 +0000

    MAJ du programme

--- src/batch/8-monitoring_retraining_requirements.py
+++ src/batch/8-monitoring_retraining_requirements.py
@@ -11,6 +11,7 @@ data_dir = dirname(dirname(abspath(__file__))) + "/data/pred_model/"
 
 model_perf = pd.read_json(data_dir+'model_metrics_bretagne_db.json')
 model_perf.sort_values(by='date_model',inplace=True)
+model_perf = model_perf.tail(6)
 
 model_drift = pd.read_json(data_dir+'model_drift_bretagne_db.json')
 model_drift.sort_values(by='date_model',inplace=True)
@@ -23,4 +24,4 @@ print(model_drift.iloc[:,2:])
 if model_perf['pct_dist_euclid'].mean().round() > 40 or model_drift['pct_dist_euclid'].mean() > 30:
     subprocess.call(['python3', '9-model_retraining.py'])
 else :
-    print('Pas de ré-entraînement')
+    print('Au vu de ces résultats aucun ré-entraînement n\'est nécessaire')
